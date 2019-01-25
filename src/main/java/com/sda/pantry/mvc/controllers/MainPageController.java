e57b9f5f53d7e47ddaa9b68777382315ef6c338b
Aleksandra Kamieniarz <ola.kamieniarz2@gmail.com>
2019-01-25 11:22:38 +0100

    new view added

---
+++ src/main/java/com/sda/pantry/mvc/controllers/MainPageController.java
@@ -0,0 +1,13 @@
+package com.sda.pantry.mvc.controllers;
+
+import com.sda.pantry.core.services.RecipeService;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class RecipeController {
+
+    private RecipeService recipeService;
+
+
+    public String prepareMainPage
+}
